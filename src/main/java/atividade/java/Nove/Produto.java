a4efc7bcf5fc4f298a4edb898e0915ce49cd84a8
Pedro <nunespedro03@gmail.com>
2022-02-20 00:36:42 -0300

    Criação da classe Produto

--- src/main/java/atividade/java/Nove/Produto.java
+++ src/main/java/atividade/java/Nove/Produto.java
@@ -1,5 +1,14 @@
 package atividade.java.Nove;
 
+import java.sql.Date;
+
 public class Produto {
+
+	private int id;
+	private String nome;
+	private double desconto;
+	private Date data_inicio;
+	
+	
 
 }
