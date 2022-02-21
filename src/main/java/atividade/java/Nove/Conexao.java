09a358a022e7e25ff9c965be7057422d9fb48792
Pedro <nunespedro03@gmail.com>
2022-02-21 02:09:13 -0300

    Atualizando a conexao

--- src/main/java/atividade/java/Nove/Conexao.java
+++ src/main/java/atividade/java/Nove/Conexao.java
@@ -5,8 +5,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexao {
-	public Connection conexao() throws SQLException {
+	public static Connection conexao() throws SQLException {
 		return DriverManager.getConnection("jdbc:mysql://localhost/x_gamer?useTimezone=true&serverTimezone=UTC", "root", "phnd0905");
 		
+	}
+	public static void main(String[] args) throws SQLException {
+		conexao();
+		System.out.println("conexao realizada");
 	}
 }
